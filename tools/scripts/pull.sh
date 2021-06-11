4757200d354af4621dba66607dd1f23c43763817
jokergong <jokergong@tencent.com>
2021-06-11 15:23:20 +0800

    pull

--- tools/scripts/pull.sh
+++ tools/scripts/pull.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 # 拉取逻辑：
-# 1. 执行svn update，成功执行3，如果没有GameData目录执行2
-# 2. svn co xxx，如果这一步失败了则不往下进行，成功执行进入3
-# 3. git pull
+# 1. 检查当前分支是否绑定了远程分支，若没有则报错退出。否则执行2。
+# 2. 执行svn update，成功执行3，如果没有GameData目录执行2
+# 3. svn co xxx，如果这一步失败了则不往下进行，成功执行进入3
+# 4. git pull
 
 
 cd $(dirname "$0")
+
+# 1. 检查当前分支是否绑定了远程分支，没有则报错退出
+CUR_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+GIT_ORIGIN_URL=`git branch -vv | grep  ${CUR_BRANCH} | grep '\[origin'`
+if [ ${#GIT_ORIGIN_URL} -ne 0 ]
+then 
+    GIT_ORIGIN_URL=`git rev-parse --abbrev-ref ${CUR_BRANCH}@{upstream}`
+else 
+    echo "[Error] 当前分支未绑定远程分支"
+    exit -1
+fi 
+echo $GIT_ORIGIN_URL
+exit 0
+
 # echo `pwd`
 GAMEDATA_PAR_DIR="../.."
 GAMEDATA_DIR="./svnlibs"
